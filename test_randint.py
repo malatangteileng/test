b223a27d28ec38ec353b377dc0d8b9c4322e26ea
malatangteileng <40053045+malatangteileng@users.noreply.github.com>
2018-07-27 21:12:36 +0800

    上传测试
    上传一个文件

---
+++ test_randint.py
@@ -0,0 +1,26 @@
+# import random
+#
+# i = 0
+# while True:
+#     i += 1
+#     a = random.randint(1, 6)
+#     print(a)
+#     if i ==100:
+#         break
+
+import math
+import numpy as np
+s = [[1, 3], [5, 6], [6,6]]
+a = np.array(s, dtype=np.float32)
+b = np.arange(6)+5
+print(b)
+s = math.sin(math.pi/6)
+print(s)
+print(math.pi)
+# print(a.shape)
+# print(a.ndim)
+# print(a.size)
+# print(a.dtype)
+# print(a.repeat(2, axis=1))
+
+
